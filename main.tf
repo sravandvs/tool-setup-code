d2077d7479dbd707a600f8d602c7d86710a7cc2c
sravandvs <sravandvs6@gmail.com>
2025-01-26 20:41:49 +0530

    Hashicorp Vault

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "tools" {
   source        = "./module-ec2"
   tool_name     = each.key
   sg_port       = each.value["port"]
-  volume_size   = each.value["port"]
+  volume_size   = each.value["volume_size"]
   instance_type = each.value["instance_type"]
   domain_name   = var.domain_name
   zone_id       = var.zone_id
