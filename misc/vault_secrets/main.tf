49f0ba20cf12f8ca887636570ffb40f9176a3711
sravandvs <sravandvs6@gmail.com>
2025-02-01 20:47:16 +0530

    Hashicorp Vault

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -14,15 +14,8 @@ provider "vault" {
 
 variable "vault_token" {}
 
-resource "vault_mount" "roboshop-dev" {
-  path      = "roboshop-dev"
-  type      = "kv"
-  options   = {version = "2" }
-  description = "Roboshop Dev Secrets"
-}
-
 resource "vault_generic_secret" "roboshop-dev" {
-  path = "${vault_mount.roboshop-dev.path}/frontend"
+  path = "roboshop-dev/frontend"
 
   data_json = <<EOT
 {
