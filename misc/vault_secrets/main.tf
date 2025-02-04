f2b99d394a72822edee549ebfcb18b10c288bc21
sravandvs <sravandvs6@gmail.com>
2025-02-04 21:58:55 +0530

    Hashicorp_vault

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -22,7 +22,7 @@ resource "vault_mount" "roboshop-dev" {
   description = "Roboshop Dev Secrets"
 }
 
-resource "vault_generic_secret" "roboshop-dev" {
+resource "vault_generic_secret" "frontend" {
   path = "${vault_mount.roboshop-dev.path}/frontend"
 
   data_json = <<EOT
@@ -32,7 +32,102 @@ resource "vault_generic_secret" "roboshop-dev" {
   "user_url": "http://user-dev.devopsdvs.online:8080/",
   "shipping_url": "http://shipping-dev.devopsdvs.online:8080/",
   "payment_url": "http://payment-dev.devopsdvs.online:8080/"
+}
+EOT
+}
 
+resource "vault_generic_secret" "catalogue" {
+  path = "${vault_mount.roboshop-dev.path}/catalogue"
+
+  data_json = <<EOT
+{
+  "MONGO: "true"
+  "MONGO_URL" : "mongodb://mongodb-dev.devopsdvs.online:27017/catalogue"
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "user" {
+path = "${vault_mount.roboshop-dev.path}/user"
+
+data_json = <<EOT
+{
+  "MONGO: "true"
+  "MONGO_URL" : "mongodb://mongodb-dev.devopsdvs.online:27017/users",
+  "REDIS_URL" : "redis://redis-dev.devopsdvs.online:6379"
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "cart" {
+  path = "${vault_mount.roboshop-dev.path}/cart"
+
+  data_json = <<EOT
+{
+  "REDIS_HOST: "redis-dev.devopsdvs.online"
+  "CATALOGUE_HOST": "catalogue-dev.devopsdvs.online"
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "shipping" {
+  path = "${vault_mount.roboshop-dev.path}/shipping"
+
+  data_json = <<EOT
+{
+  "CART_ENDPOINT: "cart-dev.devopsdvs.online:8080",
+  "DB_HOST": "mysql-dev.devopsdvs.online",
+   "mysql_root_password" : "Roboshop@1"
+}
+EOT
+}
+
+Environment=CART_HOST=cart-{{ env }}.devopsdvs.online
+Environment=CART_PORT=8080
+Environment=USER_HOST=user-{{ env }}.devopsdvs.online
+Environment=USER_PORT=8080
+Environment=AMQP_HOST=rabbitmq-{{ env }}.devopsdvs.online
+Environment=AMQP_USER=roboshop
+Environment=AMQP_PASS=roboshop123
+
+resource "vault_generic_secret" "payment" {
+  path = "${vault_mount.roboshop-dev.path}/payment"
+
+  data_json = <<EOT
+{
+  "CART_HOST" : "cart-dev.devopsdvs.online"
+  "CART_PORT" : "8080"
+  "USER_HOST" : "user-dev.devopsdvs.online"
+  "USER_PORT" : "8080"
+  "AMQP_HOST" : "rabbitmq-dev.devopsdvs.online"
+  "AMQP_USER" : "roboshop"
+  "AMQP_PASS" : "roboshop123"
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "mysql" {
+  path = "${vault_mount.roboshop-dev.path}/mysql"
+
+  data_json = <<EOT
+{
+  "mysql_root_password" : "Roboshop@1"
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "rabbitmq" {
+  path = "${vault_mount.roboshop-dev.path}/rabbitmq"
+
+  data_json = <<EOT
+{
+  "user" : "roboshop",
+  "password" : "roboshop123"
 }
 EOT
 }
